b443df7adb156e33649fdd5f5ebed8df5bb08959
tgarkbit <vova-buchenkov@yandex.ru>
2019-07-31 09:09:59 +0300

    First 5-10 functions

--- libft.c
+++ libft.c
@@ -1,6 +1,6 @@
 #include "libft.h"
 
-void *ft_memset(void *s, int c, size_t n)
+void	*ft_memset(void *s, int c, size_t n)
 {
     int i;
 
@@ -12,7 +12,7 @@ void *ft_memset(void *s, int c, size_t n)
     return (s);
 }
 
-void ft_bzero(void *s, size_t n)
+void	ft_bzero(void *s, size_t n)
 {
 	int i;
 
@@ -21,7 +21,7 @@ void ft_bzero(void *s, size_t n)
 		((char*)s)[i - 1] = '\0';
 }
 
-void *ft_memcpy(void *dst, void *src, size_t size)
+void	*ft_memcpy(void *dst, void *src, size_t size)
 {
 	char *start;
 
@@ -36,7 +36,7 @@ void *ft_memcpy(void *dst, void *src, size_t size)
 	return (start);
 }
 
-void *ft_memmove (void *destination, void *source, size_t n)
+void	*ft_memmove (void *destination, void *source, size_t n)
 {
 	char *copy;
 	int i;
@@ -58,7 +58,40 @@ void *ft_memmove (void *destination, void *source, size_t n)
 	return (destination);
 }
 
-int     ft_strlen(char *str)
+void	*ft_memchr (void *arr, int c, size_t n)
+{
+	int i;
+
+	i = 0;
+	while(((char*)arr)[i] && (i * sizeof(char)) < n)
+	{
+		if (((char*)arr)[i] == c)
+			return (arr + i);
+		i++;
+	}
+	return (NULL);
+}
+
+int		ft_memcmp(void *buf1, void *buf2, size_t count)
+{
+	int i;
+
+	i = 0;
+	while (i != (count - 1))
+	{
+		if ((((char*)buf1)[i]) && (((char*)buf1)[i]))
+			if (((char*)buf1)[i] != ((char*)buf2)[i])
+				return (((char*)buf1)[i] > ((char*)buf2)[i] ? 1 : -1 );
+			if (!((char*)buf1)[i])
+				return (-1);
+			if (!((char*)buf2)[i])
+				return (1);
+		i++;
+	}
+	return (0);
+}
+
+int		ft_strlen(char *str)
 {
     int n;
 
@@ -68,7 +101,7 @@ int     ft_strlen(char *str)
     return (n);
 }
 
-char    *ft_strdup(char *str)
+char	*ft_strdup(char *str)
 {
 	char *cpy;
 	int i;
@@ -83,7 +116,18 @@ char    *ft_strdup(char *str)
 	return (cpy);
 }
 
-char *ft_strncpy( char * destptr, const char * srcptr, size_t num )
+char	*ft_strcpy(char *destptr, const char *srcptr)
+{
+	int i;
+
+	i = -1;
+	while (srcptr[(++i)])
+		destptr[i] = srcptr[i];
+	destptr[i] = '\0';
+	return (destptr);
+}
+
+char	*ft_strncpy( char * destptr, const char * srcptr, size_t num )
 {
 	int i;
 
@@ -95,7 +139,7 @@ char *ft_strncpy( char * destptr, const char * srcptr, size_t num )
 	return (destptr);
 }
 
-char    *ft_strcat(char *str1, char *str2)
+char	*ft_strcat(char *str1, char *str2)
 {
     int i;
     int j;
@@ -112,4 +156,22 @@ char    *ft_strcat(char *str1, char *str2)
     }
     str1[i] = '\0';
     return (str1);
+}
+
+char 	*ft_strncat(char *str1, char *str2, size_t count)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = -1;
+	while (str1[i])
+		i++;
+	while(str2[++j] && j < count)
+		str1[i++] = str2[j];
+	return (str1);
+}
+char 	*ft_strlcpy(char *dst, const char *src, size_t size)
+{
+
 }
--- libft.h
+++ libft.h
@@ -3,17 +3,18 @@
 
 #include <stdlib.h>
 
-void    *ft_memset(void *s, int c, size_t n);
-void    ft_bzero(void *s, size_t n);
-void    *ft_memcpy(void *dst, void *src, size_t n);
-void    *ft_memccpy(void *dest, const void *source, int ch, size_t count);
-void    *ft_memmove (void *destination, void *source, size_t n);
-void    *ft_memchr (const void *arr, int c, size_t n);
-int     ft_strlen(char *str);
-char    *ft_strdup(char *str);
-
-
-char    *ft_strncpy( char * destptr, const char * srcptr, size_t num );
-char    *ft_strcat(char *str1, char *str2);
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dst, void *src, size_t n);
+void	*ft_memccpy(void *dest, const void *source, int ch, size_t count);
+void	*ft_memmove (void *destination, void *source, size_t n);
+void	*ft_memchr (void *arr, int c, size_t n);
+int		ft_memcmp(void *buf1, void *buf2, size_t count);
+int		ft_strlen(char *str);
+char	*ft_strdup(char *str);
+char	*ft_strcpy(char *destptr, const char *srcptr);
+char	*ft_strncpy( char * destptr, const char * srcptr, size_t num);
+char	*ft_strcat(char *str1, char *str2);
+char 	*ft_strncat(char *str1, char *str2, size_t count);
 
 #endif
